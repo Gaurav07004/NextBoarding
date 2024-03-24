5cc614cdcf09a7caa23899de7cda696bd9936ed4
Gaurav07004 <singhgaurav07004@gmail.com>
2024-03-24 16:44:31 +0530

    first commit

--- client/src/components/JsxFolder/BookingSection.jsx
+++ client/src/components/JsxFolder/BookingSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"; // gaurav
 import { Button } from "keep-react";
 import { format } from "date-fns";
 import { NavLink } from "react-router-dom";
